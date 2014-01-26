06a0d52909bbd5a7b97aa474813bf03a655e7fb8
VitordeAquino <vitordeaquino@gmail.com>
2014-01-26 16:53:57 -0300

    add game and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "vitordeaquino@gmail.com", password: "12345678", password_confirmation: "12345678")
+User.create(email: "alissondeaquino@gmail.com", password: "12345678", password_confirmation: "12345678"
+
+Game.create(user_id: 1)
