20fc2e832eaaa3c615ef9f049bb77858e36029b5
Alisson <alisson@alisson-HP-ProBook-6465b.(none)>
2014-02-04 12:03:48 -0300

    pontos calculando direito

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,44 @@
 class UsersController < ApplicationController
 	before_filter :authenticate_user!
 	before_action :set_user, only: [:show, :edit, :update]
-
+  # after_update :atualizar_pontos, if: :current_user.
 	def index
 		@users = User.all
 	end
 
 	def show
 	end
+
+  def atualizar_pontos()
+   @users = User.all
+    @users.each do |user|
+      user.pontos = 0
+      contador = 1 
+      while(contador < 7 && current_user.games.find_by_numeroDoJogo(contador).score1 != nil)
+        if current_user.games.find_by_numeroDoJogo(contador).score1 == user.games.find_by_numeroDoJogo(contador).score1 && current_user.games.find_by_numeroDoJogo(contador).score2 == user.games.find_by_numeroDoJogo(contador).score2
+          user.pontos = user.pontos + 3
+          user.save
+    
+        elsif current_user.games.find_by_numeroDoJogo(contador).score1 > current_user.games.find_by_numeroDoJogo(contador).score2
+          if user.games.find_by_numeroDoJogo(contador).score1 > user.games.find_by_numeroDoJogo(contador).score2
+            user.pontos = user.pontos + 1
+            user.save
+          end
+        elsif current_user.games.find_by_numeroDoJogo(contador).score1 < current_user.games.find_by_numeroDoJogo(contador).score2
+          if user.games.find_by_numeroDoJogo(contador).score1 < user.games.find_by_numeroDoJogo(contador).score2
+            user.pontos = user.pontos + 1
+            user.save
+          end
+        else
+          if user.games.find_by_numeroDoJogo(contador).score1 == user.games.find_by_numeroDoJogo(contador).score2
+            user.pontos = user.pontos + 1
+            user.save
+          end
+        end
+      contador = contador + 1
+      end
+    end
+  end
 
   def palpite
     @games = User.find(current_user).games
@@ -18,6 +49,9 @@ class UsersController < ApplicationController
 
 	def update
     respond_to do |format|
+      if current_user.admin?
+          atualizar_pontos()
+      end
       if @user.update(user_params)
         format.html { redirect_to palpite_path, notice: 'User was successfully updated.' }
         format.json { head :no_content }
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(email: "admin@gmail.com", password: "12345678", password_confirmation: "12345678", admin: true)
+Game.create(time1: "Brasil", time2: "Croacia", user_id: User.find_by_email("admin@gmail.com").id, numeroDoJogo: 1)
+Game.create(time1: "Mexico", time2: "Camaroes", user_id: User.find_by_email("admin@gmail.com").id, numeroDoJogo: 2)
+Game.create(time1: "Brasil", time2: "Mexico", user_id: User.find_by_email("admin@gmail.com").id, numeroDoJogo: 3)
+Game.create(time1: "Croacia", time2: "Camaroes", user_id: User.find_by_email("admin@gmail.com").id, numeroDoJogo: 4)
+Game.create(time1: "Camaroes", time2: "Brasil", user_id: User.find_by_email("admin@gmail.com").id, numeroDoJogo: 5)
+Game.create(time1: "Croacia", time2: "Mexico", user_id: User.find_by_email("admin@gmail.com").id, numeroDoJogo: 6)
