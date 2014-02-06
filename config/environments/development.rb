75eefb0b8e4e5d29767e8624c728eafe85ca714b
VitordeAquino <vitordeaquino@gmail.com>
2014-02-06 16:42:47 -0300

    email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 BolaoOficial::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -13,9 +14,13 @@ BolaoOficial::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -27,5 +32,12 @@ BolaoOficial::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.smtp_settings = {
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'localhost:3000', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => 'vitordeaquino@gmail.com',
+    :password           => '<password>'
+    }
 end
