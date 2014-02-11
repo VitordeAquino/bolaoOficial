20b75dbf19e1eb3f3c1e06096fb137a6885442a9
VitordeAquino <vitordeaquino@gmail.com>
2014-02-11 16:52:41 -0300

    Add gem pg

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!
+  
   # GET /pins
   # GET /pins.json
   def index
