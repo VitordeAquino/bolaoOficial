3a880854da1fd44e687fad30547790e41b2df065
VitordeAquino <vitordeaquino@gmail.com>
2014-03-09 21:27:49 -0300

    Mudança no Header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 	before_action :set_user, only: [:show, :edit, :update]
 
 	def index
-		@users = User.all
+		@users = User.all.where(admin: false)
 	end
 
 	def show
