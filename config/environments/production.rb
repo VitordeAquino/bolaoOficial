d866e4e581922aac01c5c762a7102bf467f781f0
VitordeAquino <vitordeaquino@gmail.com>
2014-05-02 23:31:57 -0300

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ BolaoOficial::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "127.0.0.1",
-    :port    => 25,
+    :port    => 587,
     :domain  => 'google.com'
 }
 
