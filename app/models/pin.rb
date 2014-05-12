08a75d692a3162ebc03887d9ae3ecff5f7401201
VitordeAquino <vitordeaquino@gmail.com>
2014-05-12 16:27:29 -0300

    consertando erro do comprovante admin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, styles: {medium: "320x240>"}
+	has_attached_file :image, styles: {medium: "1024x768>"}
 	validates_attachment :image, :presence => true,
-																content_type: { content_type: ['image/jpeg','image/jpg', 'image/png', 'image/gif']},
+																content_type: { content_type: ['image/jpeg','image/jpg', 'image/png', 'image/gif', 'image/pdf']},
 																size: { less_than: 5.megabytes}
 
 
