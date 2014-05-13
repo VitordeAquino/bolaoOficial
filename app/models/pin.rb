77bcd432adba9bf8f50bd3298c0f16133d31a566
VitordeAquino <vitordeaquino@gmail.com>
2014-05-13 15:20:56 -0300

    retirando formato gif

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, styles: {medium: "1024x768>"}
 	validates_attachment :image, :presence => true,
-																content_type: { content_type: ['image/jpeg','image/jpg', 'image/png', 'image/gif']},
+																content_type: { content_type: ['image/jpeg','image/jpg', 'image/png']},
 																size: { less_than: 5.megabytes}
 
 
